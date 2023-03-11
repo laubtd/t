764a9356a237a020fe85a20956ea16a2a2fd22d4
laubtd <la.bertrand@student.uclouvain.be>
2023-03-11 16:14:33 +0100

    code expo

---
+++ p.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+  long int sum = 0, i, n = 42;
+  for (i = 1; i <= n; i++) {
+    sum += i;
+  }
+  printf("\\sum_{i=1}^{ %ld } i = %ld\n", n, sum);
+  return EXIT_SUCCESS;
+}
